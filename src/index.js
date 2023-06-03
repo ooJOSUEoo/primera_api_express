0926a7cb0fea87789111f957fa04fc28fc996bce
Josue <sjosue5082002@gmail.com>
2023-06-03 18:36:28 -0500

    deploy en vercel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const routerAPI = require('./routes/indexRoutes');
 const { logErrors, errorHandler } = require('./middlewares/errorHandle');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
@@ -19,8 +19,37 @@ const options = {
 }
 app.use(cors()); //cualquier origen
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/api', (req, res) => {
+  res.send(`
+    <style>
+      html {
+        margin: 0 50px;
+        font-family: sans-serif;
+        background-color: #1a1a1a;
+      }
+      h1, p, b, li, pre {
+        color: white;
+      }
+    </style>
+    <h1>API en Express</h1>
+    <p>Urls</p>
+    <ul>
+      <b>GET</b>
+      <li>/api/v1/products</li>
+      <br><br>  
+      <b>GET/POST/PATCH/DELETE</b>
+      <li>/api/v1/products/{id}</li>
+    </ul>
+    <br><br>
+    <p>Body</p>
+    <pre>
+      {
+        "name": "string",
+        "price": "number",
+        "image": "string"
+      }
+    </pre>
+  `);
 })
 
 routerAPI(app);
