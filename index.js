c67527d428efff767952c0392f141dccfdd8f10f
Josue <sjosue5082002@gmail.com>
2023-06-02 20:08:09 -0500

    2.3 get con parametros

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const express = require('express');
+const {faker} = require('@faker-js/faker');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+
+app.get('/products', (req, res) => {
+  const products = []
+  const {size} = req.query
+  const limit = size || 10
+  for (let i = 0; i < limit; i++) {
+    products.push({
+      name: faker.commerce.product(),
+      price: faker.commerce.price(),
+      image: faker.image.imageUrl()
+    })
+  }
+  res.json(products)
+})
+app.get('/products/filter', (req, res) => {
+  res.send('Esto es un filtro!')
+})
 app.get('/products/:id', (req, res) => {
-  const {id} = req.params;
+  const {id} = req.params
   res.json({
-    id,
-    name: "Product 1",
-    price: 100
+    name: faker.commerce.product(),
+    price: faker.commerce.price(),
+    image: faker.image.imageUrl()
   })
+})
+
+app.get('/users', (req, res) => {
+  const {limit, offset} = req.query;
+  if (limit && offset) {
+    res.json({
+      limit,
+      offset
+    })
+  } else {
+    res.json({
+      limit: 10,
+      offset: 0
+    })
+  }
 })
 
 app.get('/categories/:cid/products/:pid', (req, res) => {
