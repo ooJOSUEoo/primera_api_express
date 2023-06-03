e675d63f67c9d606fd238e210e2d5457f1347f9f
Josue <sjosue5082002@gmail.com>
2023-06-02 19:08:53 -0500

    1.4 routing con express

--- index.js
+++ index.js
@@ -5,6 +5,11 @@ const port = 3000;
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
+app.get('/test', (req, res) => {
+  res.json({
+    message: 'Hello World!'
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
