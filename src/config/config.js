eb278acca895af91dab595e2c0e89c8e41dd6239
Josue <sjosue5082002@gmail.com>
2023-07-31 16:43:52 -0500

    3.4 Generar JWT en el servicio

--- src/config/config.js
+++ src/config/config.js
@@ -11,6 +11,8 @@ const config = {
     isProd: process.env.NODE_ENV === 'production',
     dbUrl: process.env.DATABASE_URL,
     apiKey: process.env.API_KEY,
+    jwtSecret: process.env.JWT_SECRET,
+
 }
 
 module.exports = {config}
