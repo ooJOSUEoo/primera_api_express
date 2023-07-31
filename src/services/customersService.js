fefd602c6d95a77876ced4f2c982096fbeee5c93
Josue <sjosue5082002@gmail.com>
2023-07-31 16:29:43 -0500

    2.3 Implementando hashing para usuarios

--- src/services/customersService.js
+++ src/services/customersService.js
@@ -1,5 +1,7 @@
 const boom = require('@hapi/boom');
 const { models } = require('../libs/sequelize');
+const bcrypt = require('bcrypt');
+
 
 class CustomerService {
 
@@ -21,9 +23,18 @@ class CustomerService {
   }
 
   async create(data) {
-     const newCustomer = await models.Customer.create(data, {
+    const hash = await bcrypt.hash(data.user.password, 10);
+    const newData = {
+      ...data,
+      user: {
+        ...data.user,
+        password: hash
+      }
+    }
+    const newCustomer = await models.Customer.create(newData, {
        include: ['user']
      });
+     delete newCustomer.dataValues.user.dataValues.password;
     return newCustomer;
   }
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,12 +1,19 @@
 const boom = require("@hapi/boom")
 const getConnection = require("../libs/postgres")
 const {models} = require("../libs/sequelize")
+const bcrypt = require('bcrypt');
+
 
 class UserService {
     constructor(){}
 
     async create(data){
-        const newUser = await models.User.create(data)
+        const hash = await bcrypt.hash(data.password, 10);
+        const newUser = await models.User.create({
+        ...data,
+        password: hash
+        });
+        delete newUser.dataValues.password;
         return newUser
     }
 
